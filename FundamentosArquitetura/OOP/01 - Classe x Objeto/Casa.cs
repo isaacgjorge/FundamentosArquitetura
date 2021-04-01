df07c4db6343500b4f38c89fa4ed8d554d49e70e
Isaac Jorge <ijorge@globalblue.com>
2021-04-01 08:56:58 +0200

    esqueleto do projeto

---
+++ FundamentosArquitetura/OOP/01 - Classe x Objeto/Casa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Casa
+    {
+        /*
+         * Definicao de Classe 
+             Estrutura de dados/informacao que represente alguma coisa no mundo real
+         */
+
+        public class Casa 
+        {
+            public int TamanhoM2 { get; set; }
+
+            public int Andares { get; set; }
+
+            public decimal Valor { get; set; }
+
+            public int NumeroVagas { get; set; }
+        }
+
+        public class Objeto
+        {
+            public Objeto()
+            {
+                /*Definicao de Objeto
+                    Quando se tem uma classe sendo instanciada. 
+                  Ela passa a ser representada na memoria como um objeto
+                  Agora ela passa a ter valores
+
+                 */
+
+                var casa = new Casa { 
+                    TamanhoM2 = 1000,
+                    Andares = 2,
+                    Valor = 100000,
+                    NumeroVagas = 3
+                };
+            }
+        }
+    }
+}
---
+++ FundamentosArquitetura/OOP/02 - Pilares OOP/01 - EstadoComportamento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOP
+{
+    public class EsdadoComportamento
+    {
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public DateTime DataNascimento { get; set; }
+
+            public int CalcularIdade() 
+            {
+                var dataAtual = DateTime.Now;
+
+                return 0;
+                
+
+            }
+
+        }
+    }
+}
